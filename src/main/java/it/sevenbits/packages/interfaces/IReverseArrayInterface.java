1be3f1994c4f70cf15125deb0afa5efcc8cca81c
Anna Goncharova <goncharova.anna.94@mail.ru>
2016-04-09 23:01:41 +0600

    Change: File Main.java: delete throw EmptyArrayException, Variable object rename reverser

--- src/main/java/it/sevenbits/packages/interfaces/IReverseArrayInterface.java
+++ src/main/java/it/sevenbits/packages/interfaces/IReverseArrayInterface.java
@@ -1,16 +1,18 @@
 package it.sevenbits.packages.interfaces;
 
 import it.sevenbits.packages.generics.MyArrayGeneric;
+import it.sevenbits.packages.myException.EmptyArrayException;
 
 /**
  *interfaces ReverseArray class
  */
 public interface IReverseArrayInterface {
     /**
-     *reverseArray method
+     *
      * @param array reverse
      * @param <T> type
      * @return array
+     * @throws EmptyArrayException exception
      */
-   <T> MyArrayGeneric<T> reverseArray(final MyArrayGeneric<T> array);
+   <T> MyArrayGeneric<T> reverseArray(final MyArrayGeneric<T> array) throws EmptyArrayException ;
 }
